fde45d0c2d5e53d7d9970809cf71429c4c97e260
Atia <kareem.atiaa.9@gmail.com>
2025-02-02 15:52:22 +0200

    intial create

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-yellow-700">
+        <h1>Home</h1>
+      </div>
+    </>
+  );
+}
